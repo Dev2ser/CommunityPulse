28efc75cc4aa715625adf7b65ba6091636f5bd76
torimcclelland <tori9991@hotmail.com>
2025-12-29 18:57:41 -0500

    survey chat UI

--- frontend/src/Components/ResidentApp.jsx
+++ frontend/src/Components/ResidentApp.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import WelcomePage from "./WelcomePage";
+import SurveyChat from "./SurveyChat";
 
 function ResidentsApp() {
   return (
     <Routes>
       <Route path="/" element={<WelcomePage />} />
+      <Route path="/chat" element={<SurveyChat />} />
     </Routes>
   );
 }
